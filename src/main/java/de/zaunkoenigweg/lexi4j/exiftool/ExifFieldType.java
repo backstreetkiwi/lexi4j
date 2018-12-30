b1fe1f71c43b99efee2323f9db9e7254f8f3e02e
Nikolaus Winter <mail@nikolaus-winter.de>
2018-12-30 22:34:38 +0100

    Exif Field Types w/ de-/serializers

---
+++ src/main/java/de/zaunkoenigweg/lexi4j/exiftool/ExifFieldType.java
@@ -0,0 +1,92 @@
+package de.zaunkoenigweg.lexi4j.exiftool;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.function.Function;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Exif Fields that can be used via the Linux exiftool.
+ * 
+ * An Exif Field has a Type T that can be serialized to a String or deserialized from a String.
+ * 
+ * @author mail@nikolaus-winter.de
+ */
+class ExifFieldType<T> {
+
+    private static final DateTimeFormatter EXIF_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy:MM:dd HH:mm:ss");
+    
+    static ExifFieldType<Integer> THREE_DIGIT_INTEGER = new ExifFieldType<>(ExifFieldType::deserializeTreeDigitInteger, ExifFieldType::serializeTreeDigitInteger);
+    static ExifFieldType<String> STRING = new ExifFieldType<>(Function.identity(), Function.identity());
+    static ExifFieldType<LocalDateTime> DATETIME = new ExifFieldType<>(ExifFieldType::deserializeDatetime, ExifFieldType::serializeDatetime);
+
+    /**
+     * Function to deserialize field from String.
+     * Null values are allowed and may be returned.
+     */
+    private Function<String, T> deserializer;
+    
+    /**
+     * Function to serialize field to String.
+     * Null values are allowed and may be returned.
+     */
+    private Function<T, String> serializer;
+    
+    ExifFieldType(Function<String, T> deserializer, Function<T, String> serializer) {
+        this.deserializer = deserializer;
+        this.serializer = serializer;
+    }
+
+    Function<String, T> getDeserializer() {
+        return deserializer;
+    }
+
+    Function<T, String> getSerializer() {
+        return serializer;
+    }
+    
+    private static String serializeTreeDigitInteger(Integer in) {
+        if(in==null) {
+            return null;
+        }
+        if(in < 0 || in > 999) {
+            throw new IllegalArgumentException(String.format("%d must be between 0 and 999.", in));
+        }
+        return String.format("%03d", in);
+    }
+
+    private static Integer deserializeTreeDigitInteger(String in) {
+        if(StringUtils.isBlank(in)) {
+            return null;
+        }
+        if(!StringUtils.isNumeric(in)) {
+            throw new IllegalArgumentException(String.format("%s is not a numeric value.", in));
+        }
+        Integer value = Integer.parseInt(in);
+        if(value < 0 || value > 999) {
+            throw new IllegalArgumentException(String.format("%s is not a numeric value between 0 and 999.", in));
+        }
+        return value;
+    }
+
+    private static String serializeDatetime(LocalDateTime in) {
+        if(in==null) {
+            return null;
+        }
+        return EXIF_DATE_TIME_FORMATTER.format(in);
+    }
+
+    private static LocalDateTime deserializeDatetime(String in) {
+        if(StringUtils.isBlank(in)) {
+            return null;
+        }
+        try {
+            return LocalDateTime.parse(in, EXIF_DATE_TIME_FORMATTER);
+          } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException(String.format("Exif Datetime '%s' could not be parsed.", in));
+          }
+    }
+
+}
---
+++ src/test/java/de/zaunkoenigweg/lexi4j/exiftool/ExifFieldTypeTest.java
@@ -0,0 +1,91 @@
+package de.zaunkoenigweg.lexi4j.exiftool;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDateTime;
+
+import org.junit.Test;
+
+public class ExifFieldTypeTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testThreeDigitIntegerSerializationValueTooLow() {
+        ExifFieldType.THREE_DIGIT_INTEGER.getSerializer().apply(-1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testThreeDigitIntegerSerializationValueTooHigh() {
+        ExifFieldType.THREE_DIGIT_INTEGER.getSerializer().apply(1000);
+    }
+
+    @Test
+    public void testThreeDigitIntegerSerialization() {
+        assertEquals(null, ExifFieldType.THREE_DIGIT_INTEGER.getSerializer().apply(null));
+        assertEquals("000", ExifFieldType.THREE_DIGIT_INTEGER.getSerializer().apply(0));
+        assertEquals("001", ExifFieldType.THREE_DIGIT_INTEGER.getSerializer().apply(1));
+        assertEquals("012", ExifFieldType.THREE_DIGIT_INTEGER.getSerializer().apply(12));
+        assertEquals("123", ExifFieldType.THREE_DIGIT_INTEGER.getSerializer().apply(123));
+        assertEquals("999", ExifFieldType.THREE_DIGIT_INTEGER.getSerializer().apply(999));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testThreeDigitIntegerDeserializationValueTooLow() {
+        ExifFieldType.THREE_DIGIT_INTEGER.getDeserializer().apply("-1");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testThreeDigitIntegerDeserializationValueTooHigh() {
+        ExifFieldType.THREE_DIGIT_INTEGER.getDeserializer().apply("1000");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testThreeDigitIntegerDeserializationNoNumericValue() {
+        ExifFieldType.THREE_DIGIT_INTEGER.getDeserializer().apply("something");
+    }
+
+    @Test
+    public void testThreeDigitIntegerDeserialization() {
+        assertEquals(null, ExifFieldType.THREE_DIGIT_INTEGER.getDeserializer().apply(null));
+        assertEquals(null, ExifFieldType.THREE_DIGIT_INTEGER.getDeserializer().apply(""));
+        assertEquals(null, ExifFieldType.THREE_DIGIT_INTEGER.getDeserializer().apply("    "));
+        assertEquals(Integer.valueOf(0), ExifFieldType.THREE_DIGIT_INTEGER.getDeserializer().apply("0"));
+        assertEquals(Integer.valueOf(0), ExifFieldType.THREE_DIGIT_INTEGER.getDeserializer().apply("000"));
+        assertEquals(Integer.valueOf(1), ExifFieldType.THREE_DIGIT_INTEGER.getDeserializer().apply("1"));
+        assertEquals(Integer.valueOf(1), ExifFieldType.THREE_DIGIT_INTEGER.getDeserializer().apply("001"));
+        assertEquals(Integer.valueOf(12), ExifFieldType.THREE_DIGIT_INTEGER.getDeserializer().apply("12"));
+        assertEquals(Integer.valueOf(12), ExifFieldType.THREE_DIGIT_INTEGER.getDeserializer().apply("012"));
+        assertEquals(Integer.valueOf(123), ExifFieldType.THREE_DIGIT_INTEGER.getDeserializer().apply("123"));
+        assertEquals(Integer.valueOf(999), ExifFieldType.THREE_DIGIT_INTEGER.getDeserializer().apply("999"));
+    }
+
+    @Test
+    public void testStringDeserialization() {
+        assertSame("bla", ExifFieldType.STRING.getDeserializer().apply("bla"));
+    }
+
+    @Test
+    public void testStringSerialization() {
+        assertSame("bla", ExifFieldType.STRING.getSerializer().apply("bla"));
+    }
+    
+    @Test
+    public void testDatetimeSerialization() {
+        assertEquals(null, ExifFieldType.DATETIME.getSerializer().apply(null));
+        assertEquals("2018:12:24 21:55:53", ExifFieldType.DATETIME.getSerializer().apply(LocalDateTime.of(2018, 12, 24, 21, 55, 53)));
+    }
+
+    @Test
+    public void testDatetimeDeserialization() {
+        assertEquals(null, ExifFieldType.DATETIME.getDeserializer().apply(null));
+        assertEquals(null, ExifFieldType.DATETIME.getDeserializer().apply(""));
+        assertEquals(null, ExifFieldType.DATETIME.getDeserializer().apply("   "));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDatetimeDeserializationInvalidValue() {
+        ExifFieldType.DATETIME.getDeserializer().apply("something");
+    }
+
+    
+    
+}
