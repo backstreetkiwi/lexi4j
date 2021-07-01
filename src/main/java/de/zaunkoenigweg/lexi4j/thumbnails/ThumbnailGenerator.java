ed2c5445ad67e83cb5199726da243d65bb2b8810
Nikolaus Winter <mail@nikolaus-winter.de>
2021-07-01 16:40:07 +0200

    Thumbnail generation for images using ffmpeg

--- src/main/java/de/zaunkoenigweg/lexi4j/thumbnails/ThumbnailGenerator.java
+++ src/main/java/de/zaunkoenigweg/lexi4j/thumbnails/ThumbnailGenerator.java
@@ -12,17 +12,17 @@ import org.apache.commons.io.IOUtils;
 public class ThumbnailGenerator {
 
     public static void generateThumbnailFromVideo(File source, File target, int height) throws ThumbnailGeneratorException {
-    	Objects.requireNonNull(source, "The source file must not be null.");
-    	Objects.requireNonNull(target, "The target file must not be null.");
-    	if(!source.exists()) {
-    		throw new IllegalArgumentException(String.format("The source file %s does not exist.", source));
-    	}
-    	if(target.exists()) {
-    		throw new IllegalArgumentException(String.format("The target file %s does already exist.", target));
-    	}
-    	if(height < 0 || height > 1000) {
-    		throw new IllegalArgumentException(String.format("The height is %d but must be between 0 and 999", height));
-    	}
+        Objects.requireNonNull(source, "The source file must not be null.");
+        Objects.requireNonNull(target, "The target file must not be null.");
+        if (!source.exists()) {
+            throw new IllegalArgumentException(String.format("The source file %s does not exist.", source));
+        }
+        if (target.exists()) {
+            throw new IllegalArgumentException(String.format("The target file %s does already exist.", target));
+        }
+        if (height < 0 || height > 1000) {
+            throw new IllegalArgumentException(String.format("The height is %d but must be between 0 and 999", height));
+        }
         try {
             ProcessBuilder builder = new ProcessBuilder();
             String command = String.format("ffmpeg -y -i %s -ss 00:00:00 -vf scale=-1:%d -vframes 1 %s", source, height, target);
@@ -32,14 +32,47 @@ public class ThumbnailGenerator {
             InputStream inputStream = process.getInputStream();
             String output = IOUtils.readLines(inputStream, StandardCharsets.UTF_8).stream().collect(Collectors.joining("\n"));
             int exitCode = process.waitFor();
-            if(exitCode!=0) {
-            	throw new ThumbnailGeneratorException(String.format("ffmpeg exited with code %d:%n%n%s%n", exitCode, output));
+            if (exitCode != 0) {
+                throw new ThumbnailGeneratorException(String.format("ffmpeg exited with code %d:%n%n%s%n", exitCode, output));
             }
         } catch (IOException | InterruptedException e) {
             throw new ThumbnailGeneratorException("System call to ffmpeg failed", e);
         }
-        if(!target.exists()) {
-        	throw new ThumbnailGeneratorException("No thumbnail was generated for unknown reasons.");
+        if (!target.exists()) {
+            throw new ThumbnailGeneratorException("No thumbnail was generated for unknown reasons.");
         }
     }
+
+    public static void generateThumbnailFromImage(File source, File target, int height) throws ThumbnailGeneratorException {
+        Objects.requireNonNull(source, "The source file must not be null.");
+        Objects.requireNonNull(target, "The target file must not be null.");
+        if (!source.exists()) {
+            throw new IllegalArgumentException(String.format("The source file %s does not exist.", source));
+        }
+        if (target.exists()) {
+            throw new IllegalArgumentException(String.format("The target file %s does already exist.", target));
+        }
+        if (height < 0 || height > 1000) {
+            throw new IllegalArgumentException(String.format("The height is %d but must be between 0 and 999", height));
+        }
+        try {
+            ProcessBuilder builder = new ProcessBuilder();
+            String command = String.format("ffmpeg -y -i %s -vf scale=-1:%d %s", source, height, target);
+            builder.command("sh", "-c", command);
+            builder.redirectErrorStream(true);
+            Process process = builder.start();
+            InputStream inputStream = process.getInputStream();
+            String output = IOUtils.readLines(inputStream, StandardCharsets.UTF_8).stream().collect(Collectors.joining("\n"));
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                throw new ThumbnailGeneratorException(String.format("ffmpeg exited with code %d:%n%n%s%n", exitCode, output));
+            }
+        } catch (IOException | InterruptedException e) {
+            throw new ThumbnailGeneratorException("System call to ffmpeg failed", e);
+        }
+        if (!target.exists()) {
+            throw new ThumbnailGeneratorException("No thumbnail was generated for unknown reasons.");
+        }
+    }
+
 }
--- src/test/java/de/zaunkoenigweg/lexi4j/thumbnails/ThumbnailGeneratorTest.java
+++ src/test/java/de/zaunkoenigweg/lexi4j/thumbnails/ThumbnailGeneratorTest.java
@@ -9,56 +9,78 @@ import static org.junit.Assert.assertTrue;
 
 public class ThumbnailGeneratorTest {
 
-	private File baseFolder = new File(getClass().getResource("/thumbnails/").getPath());
-	
-    @Test(expected=IllegalArgumentException.class)
-    public void testGenerateThumbnailSrcDoesNotExist() throws ThumbnailGeneratorException {
-    	File source = new File(baseFolder, "not_existing.mov"); 
-    	File target = new File(baseFolder, "not_existing.jpg"); 
-    	ThumbnailGenerator.generateThumbnailFromVideo(source, target, 300);
+    private File baseFolder = new File(getClass().getResource("/thumbnails/").getPath());
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGenerateThumbnailFromVideoSrcDoesNotExist() throws ThumbnailGeneratorException {
+        File source = new File(baseFolder, "not_existing.mov");
+        File target = new File(baseFolder, "not_existing.jpg");
+        ThumbnailGenerator.generateThumbnailFromVideo(source, target, 300);
     }
 
-    @Test(expected=IllegalArgumentException.class)
-    public void testGenerateThumbnailTargetDoesExist() throws ThumbnailGeneratorException {
-    	File source = new File(baseFolder, "mov.mov"); 
-    	File target = new File(baseFolder, "existing_target.jpg"); 
-    	ThumbnailGenerator.generateThumbnailFromVideo(source, target, 300);
+    @Test(expected = IllegalArgumentException.class)
+    public void testGenerateThumbnailFromVideoTargetDoesExist() throws ThumbnailGeneratorException {
+        File source = new File(baseFolder, "mov.mov");
+        File target = new File(baseFolder, "existing_target.jpg");
+        ThumbnailGenerator.generateThumbnailFromVideo(source, target, 300);
     }
 
     @Test
     public void testGenerateThumbnailForMov() throws ThumbnailGeneratorException {
-    	File source = new File(baseFolder, "mov.mov"); 
-    	File target = new File(baseFolder, "mov.jpg"); 
-    	assertFalse(target.exists());
-    	ThumbnailGenerator.generateThumbnailFromVideo(source, target, 300);
-    	assertTrue(target.exists());
+        File source = new File(baseFolder, "mov.mov");
+        File target = new File(baseFolder, "mov.jpg");
+        assertFalse(target.exists());
+        ThumbnailGenerator.generateThumbnailFromVideo(source, target, 300);
+        assertTrue(target.exists());
     }
 
     @Test
     public void testGenerateThumbnailForMp4() throws ThumbnailGeneratorException {
-    	File source = new File(baseFolder, "mp4.mp4"); 
-    	File target = new File(baseFolder, "mp4.jpg"); 
-    	assertFalse(target.exists());
-    	ThumbnailGenerator.generateThumbnailFromVideo(source, target, 300);
-    	assertTrue(target.exists());
+        File source = new File(baseFolder, "mp4.mp4");
+        File target = new File(baseFolder, "mp4.jpg");
+        assertFalse(target.exists());
+        ThumbnailGenerator.generateThumbnailFromVideo(source, target, 300);
+        assertTrue(target.exists());
     }
 
     @Test
     public void testGenerateThumbnailForAvi() throws ThumbnailGeneratorException {
-    	File source = new File(baseFolder, "avi.avi"); 
-    	File target = new File(baseFolder, "avi.jpg"); 
-    	assertFalse(target.exists());
-    	ThumbnailGenerator.generateThumbnailFromVideo(source, target, 300);
-    	assertTrue(target.exists());
+        File source = new File(baseFolder, "avi.avi");
+        File target = new File(baseFolder, "avi.jpg");
+        assertFalse(target.exists());
+        ThumbnailGenerator.generateThumbnailFromVideo(source, target, 300);
+        assertTrue(target.exists());
     }
 
     @Test
     public void testGenerateThumbnailForMpeg() throws ThumbnailGeneratorException {
-    	File source = new File(baseFolder, "mpg.mpg"); 
-    	File target = new File(baseFolder, "mpg.jpg"); 
-    	assertFalse(target.exists());
-    	ThumbnailGenerator.generateThumbnailFromVideo(source, target, 300);
-    	assertTrue(target.exists());
+        File source = new File(baseFolder, "mpg.mpg");
+        File target = new File(baseFolder, "mpg.jpg");
+        assertFalse(target.exists());
+        ThumbnailGenerator.generateThumbnailFromVideo(source, target, 300);
+        assertTrue(target.exists());
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testGenerateThumbnailFromImageSrcDoesNotExist() throws ThumbnailGeneratorException {
+        File source = new File(baseFolder, "not_existing.jpg");
+        File target = new File(baseFolder, "thumbnail.jpg");
+        ThumbnailGenerator.generateThumbnailFromImage(source, target, 300);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGenerateThumbnailFromImageTargetDoesExist() throws ThumbnailGeneratorException {
+        File source = new File(baseFolder, "NikonD70.jpg");
+        File target = new File(baseFolder, "existing_target.jpg");
+        ThumbnailGenerator.generateThumbnailFromImage(source, target, 300);
+    }
+
+    @Test
+    public void testGenerateThumbnailForImage() throws ThumbnailGeneratorException {
+        File source = new File(baseFolder, "NikonD70.jpg");
+        File target = new File(baseFolder, "thumbnail.jpg");
+        assertFalse(target.exists());
+        ThumbnailGenerator.generateThumbnailFromImage(source, target, 300);
+        assertTrue(target.exists());
+    }
 }
